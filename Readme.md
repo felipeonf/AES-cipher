97aa1224374bf4e32a9887fc3ea589d9b12f4ce5
felipeonf <felipeeflorentino@gmail.com>
2023-10-25 15:40:59 -0300

    Iniciando projeto

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## Projeto 2 de Segurança Computacional
+Consiste em implementar a cifra de bloco AES, modo de operação CTR e teste em imagens(selfies).
---
+++ aes_cipher.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+Implementar essas funções
+ADD ROUND KEY
+BYTE SUB
+SHIFT ROW
+MIX COLUMN
+*/
+
+int main(void) {
+    printf("Hello World AES algorithm! \n");
+    return 0;
+}
